5d11e38cd895133f5436ddd541cfa5c99d3c4c47
rajdeo prasad <raj@gmail.com>
2021-01-06 09:56:15 +0530

    first commit

---
+++ mastNigeria/signupfront/signupbackend/routes/routes.js
@@ -0,0 +1,25 @@
+const { response } = require('express')
+const express = require('express')
+const router = express.Router()
+const signUpTemplateCopy = require('../models/SignUpModel')
+const bcrypt = require('bcrypt')
+
+router.post('/signup', async(request, response) => {
+  const saltPassword = await bcrypt.genSalt(10)
+  const securePassword = await bcrypt.hash(request.body.password, saltPassword)
+
+  const signedUpUser = new signUpTemplateCopy({
+    fullName: request.body.fullName,
+    userName: request.body.userName,
+    email: request.body.email,
+    password: securePassword
+  })
+  signedUpUser.save()
+    .then(data => {
+    response.json(data)
+    })
+    .catch(error => {
+    response.json(error)
+  })
+})
+module.exports = router
